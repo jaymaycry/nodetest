806c2e5d1ca351d508b9c2327b402fb5974c9f75
Jay Péclard <peclajea@students.zhaw.ch>
2016-03-04 18:19:26 +0100

    added country-code check for investors

--- epilogue/investor-middle.js
+++ epilogue/investor-middle.js
@@ -1,6 +1,15 @@
+var lookup         = require('country-data').lookup
+
 module.exports = {
     create: {
         write: {
+            before: function(req, res, context) {
+                var nationality = req.body.nationality
+                if (lookup.countries({alpha3: nationality}).length != 0) {
+                    return context.continue;
+                }
+                return context.error(422, "investors nationality has to be correct."); 
+            },
             after: function(req, res, context) {
                 var deals = req.body.deals;
                 if (deals) {
@@ -12,6 +21,13 @@ module.exports = {
     },
     update: {
         write: {
+            before: function(req, res, context) {
+                var nationality = req.body.nationality
+                if (lookup.countries({alpha3: nationality}).length != 0) {
+                    return context.continue;
+                }
+                return context.error(422, "investors nationality has to be correct."); 
+            },
             after: function(req, res, context) {
                 var deals = req.body.deals;
                 if (deals) {
